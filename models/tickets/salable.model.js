58b4f487377e2cbbbd5a2080db9cbf3088d60b6a
abhijit1978 <contact.me.abhijit@gmail.com>
2021-08-29 18:37:52 +0530

    Salable tickets

--- models/tickets/salable.model.js
+++ models/tickets/salable.model.js
@@ -4,13 +4,19 @@ const salableTicketSchema = mongoose.Schema({
   pnr: { type: String, require: true, minlength: 3, maxlength: 20 },
   flightNumber: { type: String, require: true, minlength: 3, maxlength: 10 },
   airlineName: { type: String, require: true, minlength: 3, maxlength: 50 },
-  location: { type: String, require: true, minlength: 3, maxlength: 50 },
+  location: {
+    locationName: { type: String, require: true, minlength: 3, maxlength: 50 },
+    locationCode: { type: String, require: true, minlength: 3, maxlength: 50 },
+  },
   travelDate: { type: Date, require: true },
   departureTime: { type: String, require: true },
   arrivalTime: { type: String, require: true },
-  salableTickets: { type: Number, require: true, minlength: 1 },
-  startDateToSale: { type: Date, require: true },
-  salePrice: { type: Number, require: true, minlength: 3 },
+  salable: {
+    startDate: { type: Date, require: true },
+    endDate: { type: Date, require: true },
+    qty: { type: Number, require: true, minlength: 1 },
+    salePrice: { type: Number, require: true, minlength: 3 },
+  },
 });
 
 const SalableTicketModel = mongoose.model("salableTicket", salableTicketSchema);
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,29 +1,101 @@
 const express = require("express");
 const router = express.Router();
 const PurchaseModel = require("../models/tickets/purchase.model");
+const SalableTicketModel = require("../models/tickets/salable.model");
 const StockModel = require("../models/tickets/stock.model");
+const Moment = require("moment");
 
 // Find all tickets, sorted on Travel Date
 async function getTickets(paramsObj) {
-  const params = {
-    travelDate: {
-      $gt: new Date(),
-    },
-  };
+  const params = {};
   for (key in paramsObj) {
     if (paramsObj[key]) params[key] = paramsObj[key];
   }
-  return await PurchaseModel.find(params).sort({ travelDate: 1 });
+  return await PurchaseModel.find(params).sort({ travelDate: -1 });
+}
+
+async function getSalableTickets(paramsObj) {
+  const params = {
+    "salable.startDate": { $lte: Moment().format("YYYY-MM-DD") },
+    "salable.endDate": { $gte: Moment().format("YYYY-MM-DD") },
+  };
+  for (key in paramsObj) {
+    if (key === "locationCode") {
+      params["location.locationCode"] = paramsObj.locationCode;
+    }
+    if (
+      key === "travelDate" &&
+      paramsObj[key] !== "Invalid date" &&
+      paramsObj[key]
+    ) {
+      params["travelDate"] = paramsObj.travelDate ? paramsObj.travelDate : "";
+    }
+  }
+  return await SalableTicketModel.find(params).sort({
+    travelDate: 1,
+    departureTime: 1,
+  });
 }
 
 async function findTickets(pnr) {
   return await PurchaseModel.find({ pnr });
 }
 
+async function findInSalable(data) {
+  const ticket = await SalableTicketModel.find({ pnr: data.pnr });
+  if (ticket) {
+    const salable = {
+      startDate: data.startDate,
+      endDate: data.endDate,
+      qty: data.qty,
+      salePrice: data.salePrice,
+    };
+    return SalableTicketModel.findOneAndUpdate(
+      { pnr: data.pnr },
+      { $set: { salable } },
+      { new: true }
+    );
+  } else {
+    return false;
+  }
+}
+
+async function isSalable(startDate, endDate, pnr, qty) {
+  const ticket = await PurchaseModel.find({ pnr });
+
+  if (new Date(ticket[0].travelDate) >= new Date(startDate)) {
+    const stock = await StockModel.find({ pnr });
+
+    if (stock[0].inHand >= qty) {
+      return {
+        found: true,
+        ticket,
+        stock,
+      };
+    } else {
+      return {
+        found: false,
+        message: "Invalid ticket as qty not in range",
+      };
+    }
+  } else {
+    return {
+      found: false,
+      message: "Invalid ticket as date not in range",
+    };
+  }
+}
+
 // Route - Get all Tickets
 router.post("/", async function (req, res, next) {
   const paramsObj = ({ airlineName, location } = { ...req.body });
   const tickets = await getTickets(paramsObj);
+  res.status(200).send(tickets);
+});
+
+// Route - Get all salable Tickets
+router.post("/getsalable", async function (req, res, next) {
+  const tickets = await getSalableTickets(req.body);
   res.status(200).send(tickets);
 });
 
@@ -74,6 +146,48 @@ router.post("/purchase", async (req, res, next) => {
     newStock.save();
   } else {
     res.status(400).send("PNR already exists");
+  }
+});
+
+// Route - Allow to sale ticket
+router.post("/salable", async (req, res, next) => {
+  const alreadySalableAndUpdated = await findInSalable(req.body);
+
+  if (alreadySalableAndUpdated) {
+    res.status(200).send("Updated Successfully");
+  } else {
+    const { startDate, endDate, pnr, qty } = { ...req.body };
+    const ticket = await isSalable(startDate, endDate, pnr, qty);
+    if (ticket.found) {
+      const newTicket = new SalableTicketModel({
+        pnr: req.body.pnr,
+        flightNumber: req.body.flightNumber,
+        airlineName: req.body.airlineName,
+        location: {
+          locationName: req.body.locationName,
+          locationCode: req.body.locationCode,
+        },
+        travelDate: req.body.travelDate,
+        departureTime: req.body.departureTime,
+        arrivalTime: req.body.arrivalTime,
+        salable: {
+          startDate: req.body.startDate,
+          endDate: req.body.endDate,
+          qty: req.body.qty,
+          salePrice: req.body.salePrice,
+        },
+      });
+
+      newTicket.save((err) => {
+        if (err) {
+          res.send({ erroMessage: "Some error", status: 500, error: err });
+        } else {
+          res.status(200).send({ message: "Sale is allowed for this ticket" });
+        }
+      });
+    } else {
+      res.status(400).send(ticket.message);
+    }
   }
 });
 
